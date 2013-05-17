9c538365b9d2131326091bc3ef60bea834d7138d
Anil Madhavapeddy <anil@recoil.org>
2013-05-17 10:28:58 +0100

    [perf] add -O3 to stub build

--- tests/perf/run.sh
+++ tests/perf/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
-ocamlopt unix.cmxa clib.c cstub.c time.ml int_direct.ml -o direct
-ocamlfind ocamlopt -verbose -linkpkg -package unix -package ctypes clib.c cstub.c time.ml int_indirect.ml -o indirect
+ocamlopt -ccopt -O3 unix.cmxa clib.c cstub.c time.ml int_direct.ml -o direct
+ocamlfind ocamlopt -ccopt -O3 -verbose -linkpkg -package unix -package ctypes clib.c cstub.c time.ml int_indirect.ml -o indirect
 ./direct
 ./indirect
 rm -f direct indirect *.so *.cmx *.cmi *.o
