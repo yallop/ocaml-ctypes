e8737130752e9622c2004546f86d6ac6d3b41e83
Anil Madhavapeddy <anil@recoil.org>
2013-05-17 10:27:00 +0100

    [perf] add Ffi_raw test

--- tests/perf/int_indirect.ml
+++ tests/perf/int_indirect.ml
@@ -1,9 +1,23 @@
-open Ctypes.Ffi.C
-open Type
-let add = foreign "add_stuff_up" (int @-> returning int)
+open Ctypes
+
+let add =
+  Ffi.C.(Type.(foreign "add_stuff_up" (int @-> returning int)))
+
+let add_raw =
+  Ffi_raw.(Types.(
+    let callspec = allocate_callspec () in
+    let arg_1_offset = Ffi_raw.add_argument callspec int in
+    let () = prep_callspec callspec int in
+    let dlfabs = Dl.dlsym "add_stuff_up" in
+    fun x -> 
+      call dlfabs callspec
+        (write int ~offset:arg_1_offset x)
+        (read int ~offset:0)
+  ))
 
 open Time
 open Printf 
 let () =
    printf "indirect_add           : %.4f\n" (time add);
+   printf "indirect_add_raw       : %.4f\n" (time add_raw);
    ()
