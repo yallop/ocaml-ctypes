1d7abaa551faacb14e7683f327c3855e40cbd2ae
Jeremy Yallop <yallop@gmail.com>
2013-05-28 18:16:43 +0100

    Test the non-error path in errno-setting calls.

--- tests/test_errno.ml
+++ tests/test_errno.ml
@@ -11,8 +11,28 @@ let test_errno_exception_raised () =
     (fun () -> fdopendir (-300))
     
 
+(*
+  Call chdir() with a valid directory path and check that zero is returned. 
+*)
+let test_int_return_errno_exception_raised () =
+  let chdir = foreign "chdir" (string @-> syscall int) in
+  assert_raises (Unix.Unix_error(Unix.ENOENT, "chdir", ""))
+    (fun () -> chdir "/unlikely_to_exist")
+    
+
+(*
+  Call chdir() with a valid directory path and check that zero is returned. 
+*)
+let test_errno_no_exception_raised () =
+  let chdir = foreign "chdir" (string @-> syscall int) in
+  assert_equal 0 (chdir (Sys.getcwd ()))
+
+    
+
 let suite = "errno tests" >:::
   ["Exception from fdopendir" >:: test_errno_exception_raised;
+   "Exception from chdir" >:: test_int_return_errno_exception_raised;
+   "No exception from chdir" >:: test_errno_no_exception_raised;
   ]
 
 
