1b71f32378dd9a33da1d9dddddb85a90935807db
Jeremy Yallop <yallop@gmail.com>
2013-06-05 00:50:05 +0100

    Documentation for the Dl module.

--- src/dl.mli
+++ src/dl.mli
@@ -1,7 +1,19 @@
+(*
+ * Copyright (c) 2013 Jeremy Yallop.
+ *
+ * This file is distributed under the terms of the MIT License.
+ * See the file LICENSE for details.
+ *)
+
+(** Bindings to the dlopen / dlsym interface. *)
+
 type library
+(** The type of dynamic libraries, as returned by {!dlopen}. *)
 
 exception DL_error of string
+(** An error condition occurred when calling {!dlopen}, {!dlclose} or {!dlsym}.  The argument is the string returned by the [dlerror] function. *)
 
+(** Flags for {!dlopen} *)
 type flag = 
     RTLD_LAZY
   | RTLD_NOW
@@ -11,5 +23,10 @@ type flag =
   | RTLD_DEEPBIND
 
 val dlopen : ?filename:string -> flags:flag list -> library
+(** Open a dynamic library. *)
+
 val dlclose : handle:library -> unit
+(** Close a dynamic library. *)
+
 val dlsym : ?handle:library -> symbol:string -> Ctypes_raw.immediate_pointer
+(** Look up a symbol in a dynamic library. *)
