c4f03fe50502a64cb4c0e970f08b3c6c68e37abe
Jeremy Yallop <yallop@gmail.com>
2013-06-05 01:31:58 +0100

    Documentation for signed integers.

--- src/signed.mli
+++ src/signed.mli
@@ -5,20 +5,44 @@
  * See the file LICENSE for details.
  *)
 
+(** Types and operations for signed integers. *)
+
 module type S = sig
   include Unsigned.S
 
   val neg : t -> t
+  (** Unary negation. *)
+
   val abs : t -> t
+  (** Return the absolute value of its argument. *)
+
   val minus_one : t
+  (** The value -1 *)
+
   val min_int : t
+  (** The smallest representable integer. *)
+
   val shift_right_logical : t -> int -> t
+(** {!shift_right_logical} [x] [y] shifts [x] to the right by [y] bits.  See
+    {!Int32.shift_right_logical}. *)
+
 end
+(** Signed integer operations *)
 
 module Int32 : S with type t = int32
+(** Signed 32-bit integer type and operations. *)
+
 module Int64 : S with type t = int64
+(** Signed 64-bit integer type and operations. *)
+
 module Long : S
+(** The signed long integer type and operations. *)
+
 module LLong : S
+(** The signed long long integer type and operations. *)
 
 type long = Long.t
+(** The signed long integer type. *)
+
 type llong = LLong.t
+(** The signed long long integer type. *)
