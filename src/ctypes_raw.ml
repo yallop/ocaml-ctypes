44afbacb9e8e35dc6b95a37fe22f43b79a96de6f
Jeremy Yallop <yallop@gmail.com>
2013-08-16 01:46:31 +0100

    Move primitive names to a function.

--- src/ctypes_raw.ml
+++ src/ctypes_raw.ml
@@ -15,7 +15,6 @@ sig
   type 'a ctype_io
   type 'a ctype = {
     raw: 'a ctype_io;
-    name: string;
     passable: bool;
   }
 
@@ -61,31 +60,28 @@ struct
 
   type 'a ctype = {
     raw: 'a ctype_io;
-    name: string;
     passable: bool;
   }
 
-
-  let initialize ?(passable=true) name raw = 
-    { raw; name; passable }
+  let initialize ?(passable=true) raw = { raw; passable }
 
   external _int_type_info : unit -> int ctype_io = "ctypes_int_type_info"
-  let int = initialize "int" (_int_type_info ())
+  let int = initialize (_int_type_info ())
 
   external _camlint_type_info : unit -> int ctype_io = "ctypes_camlint_type_info"
-  let camlint = initialize "camlint" (_camlint_type_info ())
+  let camlint = initialize (_camlint_type_info ())
 
   external _long_type_info : unit -> long ctype_io = "ctypes_long_type_info"
-  let long = initialize "long" (_long_type_info ())
+  let long = initialize (_long_type_info ())
 
   external _llong_type_info : unit -> llong ctype_io = "ctypes_llong_type_info"
-  let llong = initialize "long long" (_llong_type_info ())
+  let llong = initialize (_llong_type_info ())
 
   external _nativeint_type_info : unit -> nativeint ctype_io = "ctypes_nativeint_type_info"
-  let nativeint = initialize "intnat" (_nativeint_type_info ())
+  let nativeint = initialize (_nativeint_type_info ())
 
   external _double_type_info : unit -> float ctype_io = "ctypes_double_type_info"
-  let double = initialize "double" (_double_type_info ())
+  let double = initialize (_double_type_info ())
 
   external _voidp_type_info : unit -> _ ctype_io = "ctypes_voidp_type_info"
 
@@ -94,70 +90,70 @@ struct
     | 8 -> (module Signed.Int64 : Signed.S)
     | _ -> failwith "No suitable type available to represent pointers.")
   type voidp = PtrType.t
-  let pointer = initialize "pointer" (_voidp_type_info ())
+  let pointer = initialize (_voidp_type_info ())
 
   external _void_type_info : unit -> unit ctype_io = "ctypes_void_type_info"
-  let void = initialize ~passable:false "void" (_void_type_info ())
+  let void = initialize ~passable:false (_void_type_info ())
 
   external _int8_t_type_info : unit -> int ctype_io = "ctypes_int8_t_type_info"
-  let int8_t = initialize "int8_t" (_int8_t_type_info ())
+  let int8_t = initialize (_int8_t_type_info ())
 
   external _int16_t_type_info : unit -> int ctype_io = "ctypes_int16_t_type_info"
-  let int16_t = initialize "int16_t" (_int16_t_type_info ())
+  let int16_t = initialize (_int16_t_type_info ())
 
   external _char_type_info : unit -> char ctype_io = "ctypes_char_type_info"
-  let char = initialize "char" (_char_type_info ())
+  let char = initialize (_char_type_info ())
 
   external _schar_type_info : unit -> int ctype_io = "ctypes_schar_type_info"
-  let schar = initialize "signed char" (_schar_type_info ())
+  let schar = initialize (_schar_type_info ())
 
   external _short_type_info : unit -> int ctype_io = "ctypes_short_type_info"
-  let short = initialize "short" (_short_type_info ())
+  let short = initialize (_short_type_info ())
 
   external _int32_t_type_info : unit -> int32 ctype_io = "ctypes_int32_t_type_info"
-  let int32_t = initialize "int32_t" (_int32_t_type_info ())
+  let int32_t = initialize (_int32_t_type_info ())
 
   external _int64_t_type_info : unit -> int64 ctype_io = "ctypes_int64_t_type_info"
-  let int64_t = initialize "int64_t" (_int64_t_type_info ())
+  let int64_t = initialize (_int64_t_type_info ())
 
   external _float_type_info : unit -> float ctype_io = "ctypes_float_type_info"
-  let float = initialize "float" (_float_type_info ())
+  let float = initialize (_float_type_info ())
 
   external _uchar_type_info : unit -> uchar ctype_io = "ctypes_uchar_type_info"
-  let uchar = initialize "unsigned char" (_uchar_type_info ())
+  let uchar = initialize (_uchar_type_info ())
 
   external _uint8_t_type_info : unit -> uint8 ctype_io = "ctypes_uint8_t_type_info"
-  let uint8_t = initialize "uint8_t" (_uint8_t_type_info ())
+  let uint8_t = initialize (_uint8_t_type_info ())
 
   external _uint16_t_type_info : unit -> uint16 ctype_io = "ctypes_uint16_t_type_info"
-  let uint16_t = initialize "uint16_t" (_uint16_t_type_info ())
+  let uint16_t = initialize (_uint16_t_type_info ())
 
   external _uint32_t_type_info : unit -> uint32 ctype_io = "ctypes_uint32_t_type_info"
-  let uint32_t = initialize "uint32_t" (_uint32_t_type_info ())
+  let uint32_t = initialize (_uint32_t_type_info ())
 
   external _uint64_t_type_info : unit -> uint64 ctype_io = "ctypes_uint64_t_type_info"
-  let uint64_t = initialize "uint64_t" (_uint64_t_type_info ())
+  let uint64_t = initialize (_uint64_t_type_info ())
 
   external _size_t_type_info : unit -> size_t ctype_io = "ctypes_size_t_type_info"
-  let size_t = initialize "size_t" (_size_t_type_info ())
+  let size_t = initialize (_size_t_type_info ())
 
   external _ushort_type_info : unit -> ushort ctype_io = "ctypes_ushort_type_info"
-  let ushort = initialize "unsigned short" (_ushort_type_info ())
+  let ushort = initialize (_ushort_type_info ())
 
   external _uint_type_info : unit -> uint ctype_io = "ctypes_uint_type_info"
-  let uint = initialize "unsigned int" (_uint_type_info ())
+  let uint = initialize (_uint_type_info ())
 
   external _ulong_type_info : unit -> ulong ctype_io = "ctypes_ulong_type_info"
-  let ulong = initialize "unsigned long" (_ulong_type_info ())
+  let ulong = initialize (_ulong_type_info ())
 
   external _ullong_type_info : unit -> ullong ctype_io = "ctypes_ullong_type_info"
-  let ullong = initialize "unsigned long long" (_ullong_type_info ())
+  let ullong = initialize (_ullong_type_info ())
 
   external _complex32_type_info : unit -> Complex.t ctype_io = "ctypes_float_complex_type_info"
-  let complex32 = initialize ~passable:false "float complex" (_complex32_type_info ())
+  let complex32 = initialize ~passable:false (_complex32_type_info ())
 
   external _complex64_type_info : unit -> Complex.t ctype_io = "ctypes_double_complex_type_info"
-  let complex64 = initialize ~passable:false "double complex" (_complex64_type_info ())
+  let complex64 = initialize ~passable:false (_complex64_type_info ())
 
   external _null : unit -> voidp = "ctypes_null_value"
   let null = _null ()
