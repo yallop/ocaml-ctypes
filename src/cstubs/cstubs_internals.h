3d5c1b923efb1e93a6bae83bcce6f57fd3e03bf8
Jeremy Yallop <yallop@gmail.com>
2014-01-21 01:10:32 +0000

    Add a single header file with everything needed by generated stubs.

---
+++ src/cstubs/cstubs_internals.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2014 Jeremy Yallop.
+ *
+ * This file is distributed under the terms of the MIT License.
+ * See the file LICENSE for details.
+ */
+
+#ifndef CSTUBS_INTERNALS_H
+#define CSTUBS_INTERNALS_H
+
+/* Types and functions used by generated C code. */
+
+#include "ctypes/primitives.h"
+#include "ctypes/complex_stubs.h"
+#include "ctypes/raw_pointer.h"
+#include "ctypes/managed_buffer_stubs.h"
+
+#endif /* CSTUBS_INTERNALS_H */
