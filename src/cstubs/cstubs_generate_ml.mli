dbb6d6815db30772abc7d849de12b1f9d90b37f4
Jeremy Yallop <yallop@gmail.com>
2014-01-21 01:13:40 +0000

    Generate 'external' declarations and ML wrapper functions.

---
+++ src/cstubs/cstubs_generate_ml.mli
@@ -0,0 +1,13 @@
+(*
+ * Copyright (c) 2014 Jeremy Yallop.
+ *
+ * This file is distributed under the terms of the MIT License.
+ * See the file LICENSE for details.
+ *)
+
+(* ML stub generation *)
+
+val fn : stub_name:string -> external_name:string -> Format.formatter ->
+         ('a -> 'b) Ctypes.fn -> unit
+
+val signature : string -> Format.formatter -> ('a -> 'b) Ctypes.fn -> unit
