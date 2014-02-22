547c173711d58d8d08b5b054a1c2f7e8c261c1e2
Jeremy Yallop <yallop@gmail.com>
2014-02-22 00:58:13 +0000

    A value-oriented stub-generation interface based around 'foreign'.
    The idea is to make both the generator and the generated code
    instances of the following interface
    
      sig
        type _ fn
        val foreign : string -> 'a Ctypes.fn -> 'a fn
      end
    
    For the generator `fn' is defined as unit, since the implementation of
    `foreign' writes out the code for a function rather than returning
    one.  The generated code has `type 'a fn = 'a`, and `foreign' resolves
    the name and type of the function to the instance written in the
    generation stage.
    
    The old interface is gone, along with the need to register paths for
    views and structured types.

--- src/cstubs/cstubs_public_name.mli
+++ src/cstubs/cstubs_public_name.mli
@@ -5,24 +5,13 @@
  * See the file LICENSE for details.
  *)
 
-(* Registry for views and structured types *)
+(* Publicly visible names for type values *)
 
-open Ctypes_path
-
-type paths = { value: path; typ: path }
-
-val register_paths : 'a Static.typ -> value:string -> typ:string -> unit
-val retrieve_path : 'a Static.typ -> paths
-
-exception Registration_error of string
-
-val ident_of_ml_prim : 'a Primitives.ml_prim -> path
+val ident_of_ml_prim : 'a Primitives.ml_prim -> Ctypes_path.path
 (* The type that should appear in the extern signature *)
 
-val constructor_ident_of_prim : 'a Primitives.prim -> path
+val constructor_ident_of_prim : 'a Primitives.prim -> Ctypes_path.path
 (* The path to a value that represents the primitive type *)
 
-val ident_of_fn : 'a Static.fn ->
-  ([ `Ident of path
-   | `Appl of path * 'ml_type list
-   | `Fn of 'ml_type * 'ml_type ] as 'ml_type)
+val constructor_cident_of_prim : 'a Primitives.prim -> Ctypes_path.path
+(* The path to a constructor that represents the primitive type *)
