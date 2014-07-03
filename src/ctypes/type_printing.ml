5c9c0f72ffaaa9104ddd55345b4901716824fae9
Jeremy Yallop <yallop@gmail.com>
2014-07-03 15:14:14 +0100

    Type printing for the bytes type.

--- src/ctypes/type_printing.ml
+++ src/ctypes/type_printing.ml
@@ -63,6 +63,7 @@ let rec format_typ' : type a. a typ ->
       fprintf fmt "%s%t%t" name (k `array)
         (fun fmt -> (Array.iter (Format.fprintf fmt "[%d]") dims))
     | OCaml String -> format_typ' (ptr char) k context fmt
+    | OCaml Bytes -> format_typ' (ptr char) k context fmt
     | OCaml FloatArray -> format_typ' (ptr double) k context fmt
 
 and format_fields : type a. a boxed_field list -> Format.formatter -> unit =
