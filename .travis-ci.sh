a9f0e2875492b44679030c14b3f5977310746e12
Jeremy Yallop <yallop@gmail.com>
2014-09-23 15:29:17 +0100

    Travis configuration.

---
+++ .travis-ci.sh
@@ -0,0 +1,22 @@
+OPAM_DEPENDS="ocamlfind ounit"
+case "$OCAML_VERSION,$OPAM_VERSION" in
+4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
+4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
+*) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
+esac
+echo "yes" | sudo add-apt-repository ppa:$ppa
+sudo apt-get update -qq
+sudo apt-get install -qq ocaml ocaml-native-compilers camlp4-extra opam
+export OPAMYES=1
+export OPAMVERBOSE=1
+echo OCaml version
+ocaml -version
+echo OPAM versions
+opam --version
+opam --git-version
+opam init
+opam install ${OPAM_DEPENDS}
+eval `opam config env`
+make
+make test
