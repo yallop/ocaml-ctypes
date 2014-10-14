35fa1ba7b247e60e3205016425f3362f209d1437
arirux <fdopen@ml.ignorelist.com>
2014-10-14 17:44:23 +0200

    use mingw ocaml

--- appveyor/build.sh
+++ appveyor/build.sh
@@ -4,32 +4,60 @@ set -ex
 type -p ocamlc
 ocamlc -version
 
-# libffi
+x="$(uname -m)"
+case "$x" in
+    x86_64)
+        build=x86_64-pc-cygwin
+        host=x86_64-w64-mingw32
+        MINGW_TOOL_PREFIX=x86_64-w64-mingw32-
+        ;;
+    *)
+        build=i686-pc-cygwin
+        host=i686-w64-mingw32
+        MINGW_TOOL_PREFIX=i686-w64-mingw32-
+        ;;
+esac
+
+export AR=${MINGW_TOOL_PREFIX}ar.exe
+export AS=${MINGW_TOOL_PREFIX}as.exe
+export CC=${MINGW_TOOL_PREFIX}gcc.exe
+export CPP=${MINGW_TOOL_PREFIX}cpp.exe
+export CPPFILT=${MINGW_TOOL_PREFIX}c++filt.exe
+export CXX=${MINGW_TOOL_PREFIX}g++.exe
+export DLLTOOL=${MINGW_TOOL_PREFIX}dlltool.exe
+export DLLWRAP=${MINGW_TOOL_PREFIX}dllwrap.exe
+export GCOV=${MINGW_TOOL_PREFIX}gcov.exe
+export LD=${MINGW_TOOL_PREFIX}ld.exe
+export NM=${MINGW_TOOL_PREFIX}nm.exe
+export OBJCOPY=${MINGW_TOOL_PREFIX}objcopy.exe
+export OBJDUMP=${MINGW_TOOL_PREFIX}objdump.exe
+export RANLIB=${MINGW_TOOL_PREFIX}ranlib.exe
+export RC=${MINGW_TOOL_PREFIX}windres.exe
+export READELF=${MINGW_TOOL_PREFIX}readelf.exe
+export SIZE=${MINGW_TOOL_PREFIX}size.exe
+export STRINGS=${MINGW_TOOL_PREFIX}strip.exe
+export STRIP=${MINGW_TOOL_PREFIX}strip.exe
+export WINDMC=${MINGW_TOOL_PREFIX}windmc.exe
+export WINDRES=${MINGW_TOOL_PREFIX}windres.exe
+
+# findlib is already installed
+
+# libffi:  we need a static version and only a static version
 (
+  rm -rf /opt/libffi
+  mkdir -p /opt/libffi
   wget ftp://sourceware.org/pub/libffi/libffi-3.1.tar.gz
   tar xfvz libffi-3.1.tar.gz
   cd libffi-3.1
-  (./configure  </dev/null && make </dev/null && make install </dev/null) || cat config.log
-  mkdir -p /usr/local/include/
-  ln -s -t /usr/local/include/ /usr/local/lib/libffi-3.1/include/*
+  (./configure --build="$build" --host="$host" --prefix /opt/libffi --disable-shared --enable-static </dev/null && make </dev/null && make install </dev/null) || cat config.log
 )
 
-# findlib
-(
-  wget http://download.camlcity.org/download/findlib-1.5.3.tar.gz
-  tar xvfz findlib-1.5.3.tar.gz
-  patch -p1 < appveyor/findlib-patch-create-process.patch 
-  cd findlib-1.5.3
-  ./configure && make all opt install
-)
+# ounit
+godi_add -u godi-ounit
 
-# oUnit
-(
-  wget -O ounit-2.0.0.tar.gz http://ftp.de.debian.org/debian/pool/main/o/ounit/ounit_2.0.0.orig.tar.gz
-  tar xvfz ounit-2.0.0.tar.gz
-  cd ounit-2.0.0
-  ./configure && make && make install
-)
+export LIBFFI_CFLAGS="-I/opt/libffi/lib/libffi-3.1/include"
+export LIBFFI_LIBS="-L/opt/libffi/lib -lffi"
+
 
 make all
 make -k test &>test.log || true
