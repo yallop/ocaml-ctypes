69f19946039fa50eec8d971dc316309aa1b31369
Jeremy Yallop <yallop@gmail.com>
2014-10-14 21:36:58 +0100

    Update Travis build script for 4.02.1

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -2,7 +2,7 @@ OPAM_DEPENDS="ocamlfind ounit"
 case "$OCAML_VERSION,$OPAM_VERSION" in
 4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
-4.02.0,1.1.0) ppa=avsm/ocaml42+opam11 ;;
+4.02.1,1.1.0) ppa=avsm/ocaml42+opam11 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 echo "yes" | sudo add-apt-repository ppa:$ppa
