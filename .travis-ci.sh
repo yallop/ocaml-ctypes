0891e6fa01380d491cbad94596bd9015481ca746
Jeremy Yallop <yallop@gmail.com>
2016-06-06 15:29:19 +0100

    Use the opam file to drive the Travis builds.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,14 +1,3 @@
-if test $COVERAGE -a $TRAVIS_OS_NAME != osx ; then
-    USE_BISECT=true;
-fi
-
-OPAM_DEPENDS="ocamlfind ounit lwt"
-if test $USE_BISECT ; then
-    OPAM_DEPENDS="$OPAM_DEPENDS bisect_ppx ocveralls"
-    MAKE="make COVERAGE=true"
-else
-    MAKE="make"
-fi
 case "$OCAML_VERSION" in
 4.01.0) ppa=avsm/ocaml41+opam12 ;;
 4.02.3) ppa=avsm/ocaml42+opam12 ;;
@@ -56,23 +45,17 @@ ocaml -version
 echo OPAM versions
 opam --version
 opam --git-version
-opam install ${OPAM_DEPENDS}
+
+
 eval `opam config env`
-$MAKE
-# build and run the tests
-$MAKE test
-# build and run the examples
-$MAKE run-examples
 
-if test $USE_BISECT ; then
-    ocveralls --send bisect*.out _build/bisect*.out > coveralls.json
+# Optional dependencies for coverage testing
+if test $COVERAGE -a $TRAVIS_OS_NAME != osx ; then
+    opam install bisect_ppx ocveralls
 fi
 
-# check Xen support builds too
-set -eu
-if opam install mirage-xen; then
-  make XEN=enable
-  ls -l _build/libctypes_stubs_xen.a
-else
-  echo "Mirage not installable, so not testing Xen build."
-fi
+# Optional dependencies for Xen build
+opam install mirage-xen || echo "Mirage not installable, so not testing Xen build."
+
+opam pin add -n ctypes $(pwd)
+opam install --build-test --yes ctypes
