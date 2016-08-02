d54e5a7a4f379a38c3e93584d35a3fb2de6c1813
Jeremy Yallop <yallop@gmail.com>
2016-08-02 15:19:20 +0100

    Add a ~thread_registration argument to `funptr` to support registering C threads with the OCaml runtime.

--- src/ctypes-foreign-base/ffi_call_stubs.c
+++ src/ctypes-foreign-base/ffi_call_stubs.c
@@ -140,8 +140,9 @@ static struct callspec {
   /* The context in which the call should run: whether errno is
      checked, whether the runtime lock is released, and so on. */
   struct call_context {
-    int check_errno;
-    int runtime_lock;
+    int check_errno:1;
+    int runtime_lock:1;
+    int thread_registration:1;
   } context;
 
   /* The libffi call interface structure.  It would be nice for this member to
@@ -218,11 +219,13 @@ static void populate_arg_array(struct callspec *callspec,
 
 /* Allocate a new C call specification */
 /* allocate_callspec : check_errno:bool -> runtime_lock:bool -> callspec */
-value ctypes_allocate_callspec(value check_errno, value runtime_lock)
+value ctypes_allocate_callspec(value check_errno, value runtime_lock,
+                               value thread_registration)
 {
   struct call_context context = {
     Int_val(check_errno),
     Int_val(runtime_lock),
+    Int_val(thread_registration),
   };
 
   value block = caml_alloc_custom(&callspec_custom_ops,
@@ -500,6 +503,11 @@ static void callback_handler(ffi_cif *cif,
                              void *user_data)
 {
   closure *closure = user_data;
+
+  if (closure->context.thread_registration)
+  {
+    ctypes_thread_register();
+  }
 
   if (closure->context.runtime_lock)
   {
