9d34fe22c5c823f69a82e4e2a109425e8fdfedd8
Jeremy Yallop <yallop@gmail.com>
2016-08-16 15:27:54 +0100

    Add a test for generating code with Lwt_preemptive and errno support.

---
+++ tests/test-returning-errno-lwt-preemptive/stubs/functions.ml
@@ -0,0 +1,25 @@
+(*
+ * Copyright (c) 2016 Jeremy Yallop.
+ *
+ * This file is distributed under the terms of the MIT License.
+ * See the file LICENSE for details.
+ *)
+
+(* Foreign function bindings for the errno / Lwt_preemptive tests. *)
+
+open Ctypes
+
+module Stubs (F: Cstubs.FOREIGN) =
+struct
+  open F
+
+  let struct_stat : [`stat] structure typ = structure "stat"
+  let stat = foreign "stat"
+      (string @-> ptr struct_stat @-> returning int)
+
+  let sixargs = foreign "sixargs"
+      (int @-> int @-> int @-> int @-> int @-> int @-> returning int)
+
+  let return_10 = foreign "return_10"
+      (void @-> returning int)
+end
