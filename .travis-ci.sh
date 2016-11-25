5f63bf8df3a4f89de18831191ab283f05d2fde2b
Jeremy Yallop <yallop@gmail.com>
2016-11-25 14:15:48 +0000

    Set up Android Travis build.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,3 +1,6 @@
+ANDROID_REPOSITORY=git://github.com/whitequark/opam-cross-android
+export OCAMLFINDFLAGS
+
 case "$OCAML_VERSION" in
 4.01.0) ppa=avsm/ocaml41+opam12 ;;
 4.02.3) ppa=avsm/ocaml42+opam12 ;;
@@ -17,6 +20,7 @@ install_on_linux () {
       opam init
       opam update
   fi
+  eval `opam config env`
 }
 
 install_on_osx () {
@@ -31,13 +35,30 @@ install_on_osx () {
   eval `opam config env` 
 }
 
+install_android_toolchain () {
+  sudo apt-get update -qq
+  sudo apt-get install -qq gcc-multilib
+  install_on_linux
+  opam remote add android $ANDROID_REPOSITORY
+  ARCH=arm SUBARCH=armv7 SYSTEM=linux_eabi \
+    CCARCH=arm TOOLCHAIN=arm-linux-androideabi-4.9 \
+    TRIPLE=arm-linux-androideabi LEVEL=24 \
+    STLVER=4.9 STLARCH=armeabi \
+    opam install conf-android
+  opam install ocaml-android
+  OCAMLFINDFLAGS='-toolchain android'
+}
+
 export OPAMYES=1
 export OPAMVERBOSE=1
 
 echo $TRAVIS_OS_NAME
-case $TRAVIS_OS_NAME in
-  osx) install_on_osx ;;
-  linux) install_on_linux ;;
+case $ANDROID in
+  true) install_android_toolchain ;;
+  *) case $TRAVIS_OS_NAME in
+       osx) install_on_osx ;;
+       linux) install_on_linux ;;
+     esac
 esac
 
 echo OCaml version
@@ -45,9 +66,6 @@ ocaml -version
 echo OPAM versions
 opam --version
 opam --git-version
-
-
-eval `opam config env`
 
 # Optional dependencies for coverage testing
 if test $COVERAGE -a $TRAVIS_OS_NAME != osx ; then
@@ -58,13 +76,18 @@ fi
 opam install mirage-xen || echo "Mirage not installable, so not testing Xen build."
 
 opam pin add -n ctypes $(pwd)
-opam install --build-test --yes ctypes
+if test $ANDROID; then
+	opam install --yes ctypes
+else
+	opam install --build-test --yes ctypes
+fi
 
 # Check that the inverted stubs package builds with this release
 opam pin add -n ctypes-inverted-stubs-example https://github.com/yallop/ocaml-ctypes-inverted-stubs-example.git 
-if test ! $COVERAGE && opam install --show-actions ctypes-inverted-stubs-example; then
+if test ! $ANDROID && test ! $COVERAGE && opam install --show-actions ctypes-inverted-stubs-example; then
     opam install --build-test --yes ctypes-inverted-stubs-example
 else
     echo "Pinning the inverted stubs example failed, probably due to OCaml version incompatibility"
 fi
+
 
