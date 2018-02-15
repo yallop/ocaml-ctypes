593f0bfa8b6a868986dec452ced152f3fd2a7420
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-02-15 23:09:05 +0700

    WIP 2

--- src/ctypes-foreign-base/config/discover.ml
+++ src/ctypes-foreign-base/config/discover.ml
@@ -1,5 +1,21 @@
+open Base
+open Stdio
 
 let () =
-  Configurator.main ~name:"ctypes.foreign.base" (fun c ->
-      failwith ""
-    )
+  let module C = Configurator in
+  C.main ~name:"ffi" (fun c ->
+      let default : C.Pkg_config.package_conf = {
+        libs = ["-lffi"];
+        cflags = []
+      } in
+      let conf =
+        match C.Pkg_config.get c with
+        | None -> default
+        | Some pc ->
+          Option.value (C.Pkg_config.query pc ~package:"libffi") ~default
+      in
+      let write_sexp file sexp =
+        Out_channel.write_all file ~data:(Sexp.to_string sexp)
+      in
+      write_sexp "c_flags.sexp" (sexp_of_list sexp_of_string conf.cflags);
+      write_sexp "c_library_flags.sexp" (sexp_of_list sexp_of_string conf.libs))
