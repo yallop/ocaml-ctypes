33da045d5391400c6009a07c3c46772939b93973
Anil Madhavapeddy <anil@recoil.org>
2018-12-12 15:59:09 +0000

    more fixes for dune build

--- src/ctypes-foreign-base/config/discover.ml
+++ src/ctypes-foreign-base/config/discover.ml
@@ -10,7 +10,9 @@ let () =
         match C.Pkg_config.get c with
         | None -> default
         | Some pc ->
-          Option.value (C.Pkg_config.query pc ~package:"libffi") ~default
+          (match C.Pkg_config.query pc ~package:"libffi") with
+           | None -> default
+           | Some v -> v)
       in
       let write_sexp file sexp =
         Out_channel.write_all file ~data:(Sexp.to_string sexp)
