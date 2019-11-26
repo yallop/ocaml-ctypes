ebe593af0bec45b6b8151763a1d56b7d80116c41
Matthias Horn <matthias.horn@acm.org>
2019-11-26 18:13:13 +0000

    Oops - changed in error.

--- src/ctypes-foreign-unthreaded/foreign.ml
+++ src/ctypes-foreign-unthreaded/foreign.ml
@@ -5,11 +5,4 @@
  * See the file LICENSE for details.
  *)
 
-include Ctypes_foreign_basis.Make(Ctypes_closure_properties.Make(Mutex))
-
-let () = begin
-  (* Initialize the Thread library and set up the hook for registering C
-     threads with the OCaml runtime *)
-  let _ : Thread.t = Thread.self () in
-  Ctypes_foreign_threaded_stubs.setup_thread_registration ()
-end
+include Ctypes_foreign_basis.Make(Ctypes_closure_properties.Make(Ctypes_gc_mutex))
