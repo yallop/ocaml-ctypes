2e6208b85eea94b0f8aa9dcd6c4965faef3ea183
Jeremy Yallop <yallop@gmail.com>
2021-04-24 13:01:32 +0100

    Remove Travis build files.

--- .travis-ci-arm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)
-eval $(opam config env)
-make
-make test # build and run the tests
-make examples # build and run the examples
-_build/date.native
-_build/date-cmd.native
-_build/fts-cmd.native examples
